53f7ac00ed81ab6cb10bca2881d8fbe2cda6c873
explosivejam <biohazard100000@gmail.com>
2025-05-27 14:23:19 +0300

    Working dev setup

--- src/auth_service/main.py
+++ src/auth_service/main.py
@@ -5,8 +5,13 @@ from api.v1.auth_router import v1
 
 def get_app() -> FastAPI:
     app = FastAPI()
-    app.include_router(v1)
+
+    @app.get("/")
+    def read_root():
+        return {"Hello2": "World55"}
+    # app.include_router(v1)
     return app
 
+print('lol')
 
 app = get_app()
